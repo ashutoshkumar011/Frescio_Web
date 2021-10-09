9d8849b91466d0bf312991a8084163cc69cb4e60
Amisha <gargamisha01@gmail.com>
2021-10-09 14:18:05 +0530

    registration page setup

--- project/home/views.py
+++ project/home/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    # return render(request, 'home/homepage.html' )
-    return render(request, 'home/base.html' )
+    return render( request, 'home/homepage.html' )
+    # return render(request, 'home/base.html' )
+
+def register(request):
+    return render( request, 'home/register.html' )
