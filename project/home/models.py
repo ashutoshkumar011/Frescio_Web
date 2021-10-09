ffb5d12d6ef0edc1ceaacbc2ced695c7d6b7663e
Amisha <gargamisha01@gmail.com>
2021-10-09 13:43:18 +0530

    base html for all pages added

--- project/home/models.py
+++ project/home/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.db.models.deletion import CASCADE
+from django.db.models.enums import Choices
+from slugify import slugify
 # Create your models here.
+
