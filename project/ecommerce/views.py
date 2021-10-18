dab1b1764b652b68a22925ad92ecef8de2c24802
nk183 <nk.er13.6@gmail.com>
2021-10-18 18:29:35 +0530

    fixed fert_recom and crop detail form/modal

--- project/ecommerce/views.py
+++ project/ecommerce/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-
+import csv
 # Create your views here.
 
 def sellcrops(request):
-    return render( request, 'ecommerce/sell.html')
+    context = {}
+    # context['crop'] = {'sdf','asd','lol'}
+    data=[]
+    # data = pd.read_csv("fertilizer.csv")
+    with open('home/fertilizer.csv', 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            data.append(row[1])
+    data.sort()
+    context['crop'] = data
+    return render( request, 'ecommerce/sell.html',context)
