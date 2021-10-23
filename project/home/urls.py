192720ba700abdbea3691a252b5f2b43d968b601
Amisha <gargamisha01@gmail.com>
2021-10-23 16:41:37 +0530

    fertilizer recommendation completed

--- project/home/urls.py
+++ project/home/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
 
     path('', views.home, name='homepage' ),
     path( 'fertilizer', views.fertilizer, name='fert_pred'),
-    path( 'fert_result', views.fertpred, name='fert_res')
+    path( 'fert_result', views.fertpred, name='fert_res'),
     # path('register', views.register, name='register')
     # path('register', views.register, name='register')
     # path('register', include('accounts.urls')),
--- project/home/views.py
+++ project/home/views.py
@@ -27,4 +27,60 @@ def fertilizer(request):
     return render( request, 'home/fertilizer.html',context)
 
 def fertpred(request):
-    return render( request, 'home/fertilizer_result.html')
+    if request.POST:
+        N = request.POST['nitrogen']
+        P = request.POST['phosphorous']
+        K = request.POST['pottasium']
+        PH = request.POST['ph']
+        crop_name = request.POST['cropname']
+        data=pd.read_csv('home/fertilizer.csv')
+        # data.drop("Unnamed: 0",axis=1,inplace=True)
+        n_actual = data[data['Crop'] == crop_name]['N'].iloc[0]
+        p_actual = data[data['Crop'] == crop_name]['P'].iloc[0]
+        k_actual = data[data['Crop'] == crop_name]['K'].iloc[0]
+        p_actual=  data[data['Crop'] == crop_name]['pH'].iloc[0]
+
+        n = n_actual - int(N)
+        p = p_actual - int(P)
+        k = k_actual - int(K)
+        ph = p_actual - int(PH)
+        
+        nkey=""
+        pkey=""
+        kkey=""
+        phkey=""
+        if n < -10:
+            nkey = "NHigh"
+        elif n>10:
+            nkey = "Nlow"
+        else:
+            nkey="Nok"
+            
+        if p < -10:
+            pkey = "PHigh"
+        elif p>10:
+            pkey = "Plow"
+        else:
+            pkey="Pok"
+
+        if k < -10:
+            kkey = "KHigh"
+        elif k>10:
+            kkey = "Klow"
+        else:
+            kkey="Kok"
+            
+        if ph<-10:
+            phkey="phHigh"
+        elif ph>10:
+            phkey="phLow"
+        else:
+            phkey="phok"
+        
+        context = {}
+        context['nkey'] = nkey
+        context['pkey'] = pkey
+        context['kkey'] = kkey
+        context['phkey'] = phkey
+
+        return render( request, 'home/fertilizer_result.html', context)
